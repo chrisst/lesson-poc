b62f171be52c5a4161bf8a60bed1efb93c3c70cc
Chris Stephens <chrisst@google.com>
2019-09-09 21:34:47 -0700

    initi commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,78 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'home',
+  styleUrls: ['./home.component.css'],
+  templateUrl: './home.component.html'
+})
+
+
+
+export class HomeComponent {
+  getClass(foo) {
+    if (foo['class']) {
+      console.log("zomg foo")
+      console.log(foo['class'])
+      return ["blob"].concat(foo['class'])
+    }
+    return ["blob"];
+ }
+
+  fooz = [
+    {
+      type: "text",
+      text: "first element and other ",
+      left: 10,
+      top: 20,
+      width: 100,
+      height: 50,
+      appear: 2,
+      class: ["b1"],
+    },
+    {
+      type: "text",
+      text: "second element text",
+      left: 30,
+      top: 0,
+      style: "width: 200px; height: 500px;",
+      class: ["b2"],
+      appear: 10,
+    },
+    {
+      type: "text",
+      text: "third element here",
+      // class: ["b2"],
+      appear: 10,
+      top: 100,
+    },
+    {
+      type: "image",
+      text: "holy crap an image",
+      left: 200,
+      top: 200,
+      width: 100,
+      height: 100,
+      source: 'https://i.redd.it/b4beo8zdfjl31.jpg',
+      // class: ["b2"],
+      z: 3
+    },
+    {
+      type: "image",
+      source: "https://media.giphy.com/media/mXwxPJjb1SzlhwMHfd/giphy.gif",
+      top: 250,
+      z: 2
+    }
+  ]
+
+  get foozValue () {
+    return JSON.stringify(this.fooz, null, 2);
+  }
+
+  set foozValue (v) {
+    try{
+    this.fooz = JSON.parse(v);}
+    catch(e) {
+      console.log('error occored while you were typing the JSON');
+    };
+  }
+}
